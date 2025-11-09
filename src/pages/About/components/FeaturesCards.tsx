8781b5f52dd2dabe7d9ab2d37cc4ed8aa0c966db
kobikru <kobikru@gmail.com>
2025-11-09 17:07:08 +0200

    with prettier

--- src/pages/About/components/FeaturesCards.tsx
+++ src/pages/About/components/FeaturesCards.tsx
@@ -5,10 +5,30 @@ import { IconType } from './IconTypes';
 import { bigContainerStyle, smallContainerStyle } from './styles';
 
 const cards = [
-  { id: 'card-one', icon: IconType.Target, titleKey: 'about_feature_card.card_one.title', descriptionKey: 'about_feature_card.card_one.description' },
-  { id: 'card-two', icon: IconType.File, titleKey: 'about_feature_card.card_two.title', descriptionKey: 'about_feature_card.card_two.description' },
-  { id: 'card-three', icon: IconType.ArrowGuide, titleKey: 'about_feature_card.card_three.title', descriptionKey: 'about_feature_card.card_three.description' },
-  { id: 'card-four', icon: IconType.BracketsAngle, titleKey: 'about_feature_card.card_four.title', descriptionKey: 'about_feature_card.card_four.description' },
+  {
+    id: 'card-one',
+    icon: IconType.Target,
+    titleKey: 'about_feature_card.card_one.title',
+    descriptionKey: 'about_feature_card.card_one.description',
+  },
+  {
+    id: 'card-two',
+    icon: IconType.File,
+    titleKey: 'about_feature_card.card_two.title',
+    descriptionKey: 'about_feature_card.card_two.description',
+  },
+  {
+    id: 'card-three',
+    icon: IconType.ArrowGuide,
+    titleKey: 'about_feature_card.card_three.title',
+    descriptionKey: 'about_feature_card.card_three.description',
+  },
+  {
+    id: 'card-four',
+    icon: IconType.BracketsAngle,
+    titleKey: 'about_feature_card.card_four.title',
+    descriptionKey: 'about_feature_card.card_four.description',
+  },
 ];
 
 export const FeaturesCards = () => {
@@ -18,7 +38,12 @@ export const FeaturesCards = () => {
   return (
     <div style={bigScreen ? bigContainerStyle : smallContainerStyle}>
       {cards.map((c) => (
-        <FeatureCard key={c.id} icon={c.icon} title={t(c.titleKey)} description={t(c.descriptionKey)} />
+        <FeatureCard
+          key={c.id}
+          icon={c.icon}
+          title={t(c.titleKey)}
+          description={t(c.descriptionKey)}
+        />
       ))}
     </div>
   );
