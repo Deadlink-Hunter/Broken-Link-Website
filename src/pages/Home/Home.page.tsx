c28fc04db0a1aeba7636fb7f943133f0ee6c14b5
Tamir <1tamir198@gmail.com>
2026-01-02 22:17:13 +0200

    remove welcome component

--- src/pages/Home/Home.page.tsx
+++ src/pages/Home/Home.page.tsx
@@ -1,12 +1,10 @@
 import { ColorSchemeToggle } from '@/components/ColorSchemeToggle/ColorSchemeToggle';
 import { Hero } from '@/components/Hero/Hero';
-import { Welcome } from '@/components/Welcome/Welcome';
 import { ExploreToolkit } from './Components/ExploreToolkit';
 
 export function HomePage() {
   return (
     <>
-      <Welcome />
       <Hero />
       <ExploreToolkit />
       <ColorSchemeToggle />
