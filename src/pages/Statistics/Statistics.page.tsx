f4c03264ad5fa48357016f0524286170814249b9
Nathan King <hello@nathanking.io>
2026-01-15 16:51:26 +1100

    feat(statistics): add title and subtitle to statistics page

--- src/pages/Statistics/Statistics.page.tsx
+++ src/pages/Statistics/Statistics.page.tsx
@@ -1,9 +1,18 @@
+import { useTranslation } from 'react-i18next';
 import Charts from './components/Charts';
 import { pageContainer } from './components/styles';
+import TitleBanner from './components/TitleBanner';
 
 const StatisticsPage = () => {
+  const { t } = useTranslation();
+  const titleBannerText = {
+    title: t('statistics_page.title'),
+    description: t('statistics_page.description'),
+  };
+
   return (
     <div style={pageContainer}>
+      <TitleBanner text={titleBannerText} />
       <Charts />
     </div>
   );
