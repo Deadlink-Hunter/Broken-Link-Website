1553f8b4dcab378e06b4c6cf9fbb8411ed71eff9
francescolaudato <francesco.laudato146@gmail.com>
2026-02-06 16:15:59 +0100

    refactor(Cards) - fixed Cards style using theme.ts

--- src/pages/Home/Components/ExploreToolkit.tsx
+++ src/pages/Home/Components/ExploreToolkit.tsx
@@ -1,46 +1,57 @@
 import { IconArrowRight } from '@tabler/icons-react';
 import { useTranslation } from 'react-i18next';
+import { Box, Container, SimpleGrid } from '@mantine/core';
 import { Button } from '@/components/UI/Button/Button';
 import { Card } from '@/components/UI/Card/Card';
 import { Typography } from '@/components/UI/Typography/Typography';
-import classes from '@/pages/Home/Components/ExploreToolkit.module.css';
 import { useExploreToolkitCardsData } from './useExploreToolkitCardsData';
+import { exploreToolkitStyles } from './styles';
 
-// TODO - Make this more according to the design inside base44
 export const ExploreToolkit = () => {
   const { t } = useTranslation();
   const { CARDS_DATA } = useExploreToolkitCardsData();
 
   return (
-    <div className={classes.container}>
-      <Typography variant='h1' className={classes.title}>
+    <Container style={exploreToolkitStyles.container}>
+      <Typography variant="h1" style={exploreToolkitStyles.title}>
         {t('explore_toolkit_title_part1')}
-        <span className={classes.highlight}>{t('explore_toolkit_title_highlight')}</span>
+        <span style={exploreToolkitStyles.highlight}>
+          {t('explore_toolkit_title_highlight')}
+        </span>
         {t('explore_toolkit_title_part2')}
       </Typography>
-      <Typography variant='body' className={classes.description}>
+
+      <Typography variant="body" style={exploreToolkitStyles.description}>
         {t('explore_toolkit_description')}
       </Typography>
-      <div className={classes.grid}>
+
+      <SimpleGrid cols={{ base: 1, md: 3 }} spacing="xl">
         {CARDS_DATA.map((card) => (
-          <Card key={card.id} className={classes.card}>
-            <div>
-              {card.icon && <card.icon size={60} className={classes.cardIcon} />}
-              <div className={classes.cardHeader}>
-                <Typography variant='h3' className={classes.cardTitle}>{card.titleKey}</Typography>
-              </div>
-              <Typography variant='body' className={classes.cardDescription}>
+          <Card key={card.id} style={exploreToolkitStyles.cardRoot}>
+            <Box>
+              {card.icon && (
+                <Box style={exploreToolkitStyles.cardIcon}>
+                  <card.icon size={35} />
+                </Box>
+              )}
+
+              <Typography variant="h3" style={exploreToolkitStyles.cardTitle}>
+                {card.titleKey}
+              </Typography>
+
+              <Typography variant="body" style={exploreToolkitStyles.cardDescription}>
                 {card.descriptionKey}
               </Typography>
-            </div>
+            </Box>
+
             {card.buttonTextKey && (
-              <Button variant='primary' className={classes.cardButton}>
+              <Button variant="primary" style={exploreToolkitStyles.cardButton}>
                 {card.buttonTextKey} <IconArrowRight size={18} />
               </Button>
             )}
           </Card>
         ))}
-      </div>
-    </div>
+      </SimpleGrid>
+    </Container>
   );
 };
