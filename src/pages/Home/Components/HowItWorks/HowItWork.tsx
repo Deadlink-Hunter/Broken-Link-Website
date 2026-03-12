612972612d33b9e6089e62488a03948a793d5e69
francescolaudato <francesco.laudato146@gmail.com>
2026-03-12 10:36:48 +0100

    style(HowItWorks) add step badge and icon gradient wrapper to cards

--- src/pages/Home/Components/HowItWorks/HowItWork.tsx
+++ src/pages/Home/Components/HowItWorks/HowItWork.tsx
@@ -1,8 +1,11 @@
 import { IconBolt, IconCircleCheck, IconSearch } from '@tabler/icons-react';
 import { useTranslation } from 'react-i18next';
+import { theme } from '@/theme';
 import { HowItWorkCard } from './HowItWorkCard';
 import { HowItWorkTitles } from './HowItWorkTitles';
 import { howItWorksStyles } from './styles';
+
+const colors = theme.colors;
 
 export const HowItWork = () => {
   const { t } = useTranslation();
@@ -14,17 +17,26 @@ export const HowItWork = () => {
         <HowItWorkCard
           title={t('how_it_works_card1_title')}
           description={t('how_it_works_card1_description')}
-          icon={<IconSearch size={48} />}
+          icon={<IconSearch size={40} />}
+          stepNumber={1}
+          iconFrom={colors.cyan[3]}
+          iconTo={colors.cyan[6]}
         />
         <HowItWorkCard
           title={t('how_it_works_card2_title')}
           description={t('how_it_works_card2_description')}
-          icon={<IconBolt size={48} />}
+          icon={<IconBolt size={40} />}
+          stepNumber={2}
+          iconFrom={colors.error[4]}
+          iconTo={colors.purple[5]}
         />
         <HowItWorkCard
           title={t('how_it_works_card3_title')}
           description={t('how_it_works_card3_description')}
-          icon={<IconCircleCheck size={48} />}
+          icon={<IconCircleCheck size={40} />}
+          stepNumber={3}
+          iconFrom={colors.success[3]}
+          iconTo={colors.success[6]}
         />
       </div>
     </>
--- src/pages/Home/Components/HowItWorks/HowItWorkCard.tsx
+++ src/pages/Home/Components/HowItWorks/HowItWorkCard.tsx
@@ -1,18 +1,22 @@
 import { ReactNode } from 'react';
 import { Card } from '@/components/UI/Card/Card';
 import { Typography } from '@/components/UI/Typography/Typography';
-import { howItWorksStyles } from './styles';
+import { cardIconWrapper, howItWorksStyles } from './styles';
 
 interface HowItWorkCardProps {
   title: string;
   description: string;
   icon: ReactNode;
+  stepNumber: number;
+  iconFrom: string;
+  iconTo: string;
 }
 
-export const HowItWorkCard = ({ title, description, icon }: HowItWorkCardProps) => {
+export const HowItWorkCard = ({ title, description, icon, stepNumber, iconFrom, iconTo }: HowItWorkCardProps) => {
   return (
     <Card style={howItWorksStyles.card}>
-      <div>{icon}</div>
+      <div style={howItWorksStyles.stepBadge}>{stepNumber}</div>
+      <div style={cardIconWrapper(iconFrom, iconTo)}>{icon}</div>
       <Typography variant='title'>{title}</Typography>
       <Typography variant='description'>{description}</Typography>
     </Card>
