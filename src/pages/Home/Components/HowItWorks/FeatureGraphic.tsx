13e8c9bc58097c26e0a66bdc5238db871e464fa7
unknown <jadhavraaj007@gmail.com>
2026-03-13 05:30:22 +0530

    style: rename keyframe to kebab-case to satisfy stylelint

--- src/pages/Home/Components/HowItWorks/FeatureGraphic.tsx
+++ src/pages/Home/Components/HowItWorks/FeatureGraphic.tsx
@@ -1,4 +1,4 @@
-import { IconTool, IconX, IconCheck } from '@tabler/icons-react';
+import { IconCheck, IconTool, IconX } from '@tabler/icons-react';
 import { theme } from '@/theme';
 import { howItWorksStyles } from './styles';
 import classes from './FeatureGraphic.module.css';
@@ -9,21 +9,21 @@ export const FeatureGraphic = () => {
       <div style={howItWorksStyles.graphicLineGroup}>
         <div style={howItWorksStyles.graphicLineCyan} />
         <div style={howItWorksStyles.graphicIconRed}>
-          <IconX size="1rem" color={theme.white} />
+          <IconX size='1rem' color={theme.white} />
         </div>
       </div>
-      
+
       <div style={howItWorksStyles.graphicMainIcon} className={classes.floatingIcon}>
-        <IconTool size="2.5rem" color={theme.white} />
+        <IconTool size='2.5rem' color={theme.white} />
         <div style={howItWorksStyles.graphicCheckBadge}>
-          <IconCheck size="0.75rem" color={theme.white} />
+          <IconCheck size='0.75rem' color={theme.white} />
         </div>
       </div>
 
       <div style={howItWorksStyles.graphicLineGroup}>
         <div style={howItWorksStyles.graphicLinePurple} />
         <div style={howItWorksStyles.graphicIconGreen}>
-          <IconCheck size="1rem" color={theme.white} />
+          <IconCheck size='1rem' color={theme.white} />
         </div>
       </div>
     </div>
