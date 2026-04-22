e0aa43c4742e6cd42f85d02ed14c12ef593f4e0a
moti <motigabay18@gmail.com>
2026-04-22 18:08:48 +0300

    docs: add storybook story for Navbar component

---
+++ src/components/Navbar/Navbar.story.tsx
@@ -0,0 +1,44 @@
+import type { Meta, StoryObj } from '@storybook/react';
+import { MemoryRouter } from 'react-router-dom';
+import { MantineProvider } from '@mantine/core';
+import { theme } from '@/theme';
+import Navbar from './Navbar';
+
+import '@/i18';
+
+const meta: Meta<typeof Navbar> = {
+  title: 'Components/Navbar',
+  component: Navbar,
+  parameters: {
+    layout: 'fullscreen',
+  },
+  tags: ['autodocs'],
+  decorators: [
+    (Story) => (
+      <MantineProvider theme={theme}>
+        <MemoryRouter>
+          <Story />
+        </MemoryRouter>
+      </MantineProvider>
+    ),
+  ],
+};
+
+export default meta;
+type Story = StoryObj<typeof Navbar>;
+
+export const Desktop: Story = {
+  parameters: {
+    viewport: {
+      defaultViewport: 'responsive',
+    },
+  },
+};
+
+export const Mobile: Story = {
+  parameters: {
+    viewport: {
+      defaultViewport: 'mobile1',
+    },
+  },
+};
