82310a32a6f814a82e760e9ab1d93d5c5e2afe98
Om Patel <202273921+ompatel100@users.noreply.github.com>
2026-04-28 00:42:15 +0530

    refactor: Languages, Theme for mobile and silent es lint(#458)
    - move Language Select and Theme outside hamburger for MobileNav
    - Silence es lint for allowing "s" for time seconds in SummaryBar and run it

--- src/components/Navbar/MobileNav.tsx
+++ src/components/Navbar/MobileNav.tsx
@@ -28,16 +28,7 @@ export default function MobileNav() {
           <img alt='Deadlink logo' style={styles.logoSize} src={logo} />
           <Link labelStyle={styles.header} href='/' label={t('navbar.header')} />
         </div>
-        <Burger
-          style={styles.burger}
-          size={18}
-          opened={displayLinks}
-          onClick={handleDisplayLinks}
-        />
-      </div>
-      {displayLinks && (
-        <div style={styles.linksContainer}>
-          <NavbarLinks displayLinks={displayLinks} variant='mobile' />
+        <div style={styles.buttonContainer}>
           <Select
             aria-label='Language'
             data={LANGUAGE_OPTIONS}
@@ -47,6 +38,17 @@ export default function MobileNav() {
             {...languageSelectProps}
           />
           <ThemeToggle />
+          <Burger
+            style={styles.burger}
+            size={18}
+            opened={displayLinks}
+            onClick={handleDisplayLinks}
+          />
+        </div>
+      </div>
+      {displayLinks && (
+        <div style={styles.linksContainer}>
+          <NavbarLinks displayLinks={displayLinks} variant='mobile' />
         </div>
       )}
       <Divider variant='muted' style={styles.navDivider} />
