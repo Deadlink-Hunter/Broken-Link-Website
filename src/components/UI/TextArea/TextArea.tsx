57e9b2c94c443cdd42be2ba7bba5b57df339d582
MalyRosenstein <Hand1e22@mbjcomp.org.il>
2026-09-04 13:28:46 +0300

    fix: preserve TextArea styling

--- src/components/UI/TextArea/TextArea.tsx
+++ src/components/UI/TextArea/TextArea.tsx
@@ -1,10 +1,10 @@
 import React, { CSSProperties } from 'react';
-import { MantineStyleProp, Textarea as MantineTextarea, TextareaProps } from '@mantine/core';
+import { Textarea as MantineTextarea, TextareaProps } from '@mantine/core';
 
 interface SharedTextAreaProps extends Omit<TextareaProps, 'style'> {
   style?: CSSProperties;
 }
 
 export const TextArea = ({ style = {}, ...props }: SharedTextAreaProps) => {
-  return <MantineTextarea style={style as MantineStyleProp} {...props} />;
+  return <MantineTextarea styles={{ input: style }} {...props} />;
 };
